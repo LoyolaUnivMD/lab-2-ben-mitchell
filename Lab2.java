df66bdb6d9abd0f1af2e12715755d2f54216b858
Michele Onton <maontoncueva@loyola.edu>
2024-01-31 14:02:12 -0500

    Lab2 is done

--- Lab2.java
+++ Lab2.java
@@ -1,12 +1,11 @@
-// Programmers:  [your names here]
+// Programmers:  Michele & Ben
 // Course:  CS 212
-// Due Date:
-// Lab Assignment:
-// Problem Statement:
-// Data In:
-// Data Out:
-// Credits: [Is your code based on an example in the book, in class, or something else?
-//            Reminder: you should never take code from the Internet or another person
+// Due Date: 02/04/2024
+// Lab Assignment: 2
+// Problem Statement:  write a program and generate lottery tickets for players
+// Data In: player's name
+// Data Out: Random ticket numbers
+// Credits: None
 
 import java.util.Scanner;
 import java.text.DecimalFormat;
@@ -26,16 +25,23 @@ class Lab2 {
         String customerName = input.nextLine();
 
         System.out.println("Here are the ticket:");
+        DecimalFormat df = new DecimalFormat();
         Random rand = new Random();
-        for(int i=0; i<10; i++) {
 
+        // use a for loop for the number of rows
+        for(int i=0; i<10; i++) {
+        // use a for loop to print out 6 numbers in each row
             for(int j=0; j<6; j++) {
-                System.out.print(rand.nextInt(99) + " " );
+                System.out.print(String.format("%02d",rand.nextInt(99)) + " " );
             }
             System.out.println();
         }
-
-
+        // Say goodbye to the user
+        System.out.println("-------------");
+        System.out.println("Good luck " + customerName);
+        System.out.println("Estimated Jackpot:");
+        System.out.println("$" + "225,938,745");
+        System.out.println("-------------");
 
     }
 }
